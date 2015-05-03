e76e283508b68c476cdbe1e1cb3891b47e635bdc
Daminox <devillierse.damien@gmail.com>
2015-05-03 21:49:34 +0200

    Ajout premier message WS 2, fu git

--- Front/app/scripts/modules/commons/module-commons.js
+++ Front/app/scripts/modules/commons/module-commons.js
@@ -1,10 +1,23 @@
 angular.module('commons', [])
 .controller('AccueilController',function($scope){
-    
+
 })
 .controller('FooterController',function($scope){
-    
+
 })
 .controller('HeaderController',function($scope){
-    
+
+}).service('WebSocketHelperService', function(){
+
+    var helper = {};
+
+    helper.createConnection = function(){
+        window.WebSocket = window.WebSocket || window.MozWebSocket;
+        var connection = new WebSocket('ws://127.0.0.1:1337');
+        return connection;
+    };
+
+    return helper;
+
+
 });
--- Front/app/scripts/modules/serveur/module-serveur.js
+++ Front/app/scripts/modules/serveur/module-serveur.js
@@ -1,4 +1,36 @@
 angular.module('serveur',[])
-.controller('ServeurController',function($scope){
-    
-});
+.controller('ServeurController',['ServeurService','$scope', function(ServeurService,$scope){
+    ServeurService.initConnection();
+}]).service('ServeurService',['WebSocketHelperService', function(WebSocketHelperService){
+
+    var service = {};
+
+    service.initConnection = function(callBackOpen, callBackError, callBackMessage){
+        //On commence par initialiser la connection
+        var connection = WebSocketHelperService.createConnection();
+
+        connection.onopen = callBackOpen || function(){
+            console.log('connection ouverte ! ');
+        };
+
+        connection.onerror = callBackError || function (error) {
+            console.error('error recue !');
+            console.error(error);
+        };
+
+        connection.onmessage = callBackMessage || function (message) {
+            try {
+                var json = JSON.parse(message.data);
+            } catch (e) {
+                console.error('Pas recu du json', message.data);
+                return;
+            }
+            console.log('JSON valide recu : ', json);
+        };
+
+    };
+
+
+    return service;
+
+}]);
